e0f8f39603336432af796289aec9169ba5d4bbb5
Dennis Baldwin <dennisbaldwin@gmail.com>
2023-03-21 10:17:15 -0500

    Dance example

---
+++ examples/dance.ts
@@ -0,0 +1,37 @@
+import { Go1, Go1Mode } from "../src/go1";
+
+let dog: Go1;
+
+async function main() {
+  dog = new Go1();
+
+  await dog.wait(3000);
+
+  dog.setMode(Go1Mode.stand);
+
+  await dog.wait(1000);
+
+  await dog.lookUp(0.5, 1000);
+  await dog.lookDown(0.5, 1000);
+  await dog.leanLeft(0.5, 1000);
+  await dog.leanRight(0.5, 1000);
+  await dog.twistLeft(0.5, 1000);
+  await dog.twistRight(0.5, 1000);
+
+  await dog.resetBody();
+
+  await dog.lookUp(1, 1000);
+  await dog.lookDown(1, 1000);
+  await dog.leanLeft(1, 1000);
+  await dog.leanRight(1, 1000);
+  await dog.twistLeft(1, 1000);
+  await dog.twistRight(1, 1000);
+
+  await dog.resetBody();
+
+  dog.setMode(Go1Mode.walk);
+
+  console.log("done");
+}
+
+main();
